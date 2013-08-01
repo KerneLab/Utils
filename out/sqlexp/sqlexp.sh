88bd6dc41aebb84e58dbca65207918aee7c28183
核心力量 <kernelforce@gmail.com>
2013-08-01 22:01:11 +0800

    Scripts for running sqlexp are available.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

---
+++ out/sqlexp/sqlexp.sh
@@ -0,0 +1,1 @@
+java -jar sqlexp.jar -file ./demo_table.sql -table demo_table -columns name val -keys id -rule "where id>1 order by id" -mode update -ip localhost -port 1521 -user test -pass test -db orcl -charset UTF-8
