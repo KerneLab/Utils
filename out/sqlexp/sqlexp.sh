3979cc46befd9b8691701398472a0a098b73de8e
核心力量 <kernelforce@gmail.com>
2013-08-01 22:03:10 +0800

    Add #! /bin/sh in sqlexp.sh
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- out/sqlexp/sqlexp.sh
+++ out/sqlexp/sqlexp.sh
@@ -1,1 +1,2 @@
+#! /bin/sh
 java -jar sqlexp.jar -file ./demo_table.sql -table demo_table -columns name val -keys id -rule "where id>1 order by id" -mode update -ip localhost -port 1521 -user test -pass test -db orcl -charset UTF-8
